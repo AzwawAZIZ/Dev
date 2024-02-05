18281820869d35e4fc1217159f75222543c15ba6
AzwawAZIZ <153498957+AzwawAZIZ@users.noreply.github.com>
2024-02-05 15:50:09 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+cript src="C:\Users\Mahdi\Desktop\Dev/script.js"></script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titre de votre page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <!-- Your HTML content here -->
+    
+    <script src="script.js"></script>
+</body>
+</html>
