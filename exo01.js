dbf98e56a5de95f1f49e9b8f91dff770a9c93565
AzwawAZIZ <153498957+AzwawAZIZ@users.noreply.github.com>
2024-02-08 13:02:50 +0100

    Create exo01.js

---
+++ exo01.js
@@ -0,0 +1,21 @@
+// Déclaration de variables
+let age = 25;
+const name = 'Alice';
+
+// Opérations de base
+console.log(age * 2); // Affiche le double de l'âge
+console.log(age + 5); // Affiche l'âge plus 5
+console.log(age - 10); // Affiche l'âge moins 10
+console.log(age / 5); // Affiche l'âge divisé par 5
+
+// Utilisation des instructions conditionnelles
+if (age > 18) {
+    console.log('Adulte');
+} else {
+    console.log('Mineur');
+}
+
+// Création d'une boucle
+for (let i = 1; i <= 10; i++) {
+    console.log(i); // Affiche les nombres de 1 à 10
+}
